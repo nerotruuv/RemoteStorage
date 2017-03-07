0324fbbcc1c60f01b2195a5d9014c3fd1ec2835a
meruk <mark.steijger@gmail.com>
2017-03-07 10:11:33 +0100

    l

--- CustomKeypad/CustomKeypad.ino
+++ CustomKeypad/CustomKeypad.ino
@@ -1,16 +1,9 @@
-/* @file CustomKeypad.pde
-|| @version 1.0
-|| @author Alexander Brevig
-|| @contact alexanderbrevig@gmail.com
-||
-|| @description
-|| | Demonstrates changing the keypad size and key values.
-|| #
-*/
-#include <Keypad.h>
+
+#include "Keypad.h"
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
+
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
   {'1','2','3','A'},
