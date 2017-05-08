03f0aa1de834603e63cb7062d4402162a700993e
Mark Steijger <mark.steijger@gmail.com>
2017-05-08 15:39:37 +0200

    asdr

--- java4/opd1/Stack.java
+++ java4/opd1/Stack.java
@@ -1,5 +1,18 @@
 public class Stack{
 
+	public void Push(Student su){}
+	public int Size(){}
+	public Student Pop(){}
+	public boolean Peek(Student su){}
+	public void PrintStack(){}
+	public void PrintMen(){}
+	public void PrintWoman(){}
 	
+	
+	private Student Start(){}
+	
+	private int Size(){}
 
+	
+	
 }
